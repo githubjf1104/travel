ac264b11b03e5bfb6fc34dd125cbfd65cfb8ebb9
githubjf1104 <1809546039@qq.com>
2019-10-18 13:56:38 +0800

    add recommend and weekend

---
+++ src/pages/home/components/recommend.vue
@@ -0,0 +1,81 @@
+<template>
+	<div class="recommend-title">
+		<div class="recommend-word">热销推荐</div>
+		<ul>
+			<li class="item" v-for="item of recommendList"
+			:key="item.id">
+				<img class="item-img" :src="item.imgUrl"/>
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name:'HomeRecommend',
+	data () {
+		return {
+			recommendList:[{
+				id:'0001',
+				imgUrl:'https://imgs.qunarzz.com/sight/p0/1506/e8/e87354c876fd6c38.water.jpg_200x200_9c11d922.jpg',
+				title:'北宫国家深林公园',
+				desc:'五彩斑斓，层林尽染，分分钟随手一拍就是美图'
+			},{
+				id:'0002',
+				imgUrl:'https://imgs.qunarzz.com/sight/p0/201311/12/3e2de7e7f2fa170fc8d65eac.jpg_200x200_015edb8c.jpg',
+				title:'八达岭野生动物园',
+				desc:'五彩斑斓，层林尽染，分分钟随手一拍就是美图'
+			},{
+				id:'0003',
+				imgUrl:'https://imgs.qunarzz.com/sight/p0/1602/67/67feeab24cfc82bb90.water.jpg_200x200_af2a01b1.jpg',
+				title:'太平洋海底世界',
+				desc:'五彩斑斓，层林尽染，分分钟随手一拍就是美图'
+			}]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/mixins.styl'
+	.recommend-title
+		line-height:.8rem
+		margin-top:.2rem
+		background:#eee
+		text-indent:.2rem
+	.recommend-word
+		font-size:.4rem
+	.item
+		overflow:hidden
+		display:flex
+		height:1.9rem
+		.item-img
+			width:1.7rem
+			height:1.7rem
+			padding:0 .1rem
+		.item-info
+			flex:1
+			padding:.1rem
+			min-width:0
+			.item-title
+				 line-height:.54rem
+				 font-size:.32rem
+				 ellipsis()
+			.item-desc
+				line-height:.5rem
+				color:#ccc
+				ellipsis()
+			.item-button
+				line-height:.5rem
+				background:#ff9300
+				color:#fff
+				padding:0 .3rem
+				border-radius:0.2rem
+
+				
+</style>
---
+++ src/pages/home/components/weekend.vue
@@ -0,0 +1,72 @@
+<template>
+	<div class="weekend-title">
+		<div class="weekend-word">周末去哪</div>
+		<ul>
+			<li class="item" v-for="item of weekendList"
+			:key="item.id">
+				<div class="item-img-wrapper">
+					<img class="item-img" :src="item.imgUrl"/>
+				</div>
+				<div class="item-info">
+					<p class="item-title">{{item.title}}</p>
+					<p class="item-desc">{{item.desc}}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name:'HomeWeekend',
+	data () {
+		return {
+			weekendList:[{
+				id:'0001',
+				imgUrl:'https://imgs.qunarzz.com/sight/source/1509/81/0412da9c4db66a.jpg_r_640x214_8e23871e.jpg',
+				title:'登高望远秋色佳',
+				desc:'山顶满眼彩色，此情此景醉了'
+			},{
+				id:'0002',
+				imgUrl:'https://imgs.qunarzz.com/sight/source/1505/9e/21df651e19af5d.jpg_r_640x214_3ea5bb38.jpg',
+				title:'寻找北京的皇城范儿',
+				desc:'数百年的宫廷庙宇，至今依旧威严霸气'
+			},{
+				id:'0003',
+				imgUrl:'https://imgs.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+				title:'京城周末撒欢',
+				desc:'在帝都过周末，不仅仅是城中游！'
+			}]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~@/assets/styles/mixins.styl'
+	.weekend-title
+		line-height:.8rem
+		margin-top:.2rem
+		background:#eee
+		text-indent:.2rem
+	.weekend-word
+		font-size:.4rem
+	.item-img-wrapper
+		overflow:hidden
+		height:0
+		padding-bottom:33.9%
+		.item-img
+			width:100%
+	.item-info
+		padding:.2rem
+		min-width:0
+		.item-title
+			 line-height:.54rem
+			 font-size:.32rem
+			 ellipsis()
+		.item-desc
+			line-height:.5rem
+			color:#ccc
+			ellipsis()
+				
+</style>
