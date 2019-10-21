01ba468299b412b603664628310173055020cd7b
githubjf1104 <1809546039@qq.com>
2019-10-21 10:08:01 +0800

    city components

--- src/pages/city/components/list.vue
+++ src/pages/city/components/list.vue
@@ -19,7 +19,11 @@
 					</div>
 				</div>
 			</div>
-			<div class="area" v-for="(item,key) of cities" :key="key">
+			<div class="area" 
+				v-for="(item,key) of cities" 
+				:key="key"
+				:ref="key"
+				>
 				<div class="title border-topbottom">{{key}}</div>
 				<div class="item-list">
 					<div class="item border-bottom"
@@ -41,10 +45,19 @@ export default {
 	name:'CityList',
 	props:{
 		hot:Array,
-		cities:Object	
+		cities:Object,
+		letter:String	
 	},
 	mounted () {
 		this.scroll = new Bscroll(this.$refs.wrapper)
+	},
+	watch: {
+		letter () {
+			if(this.letter){
+				const element = this.$refs[this.letter][0]
+				this.scroll.scrollToElement(element)
+			}
+		}
 	}
 
 }
